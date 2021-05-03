62da2dec257f16e8e186f71a75cdbaea93d1a249
Abdurrohman <sulthonsayid@gmail.com>
2021-05-04 01:24:15 +0700

    make a variable and user input

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ namespace C_
     {
         static void Main(string[] args)
         {
+            var input = Console.ReadLine();
+            Console.WriteLine($"You typed: {input}");
             Console.WriteLine("Hello World!"); 
         }
     }
